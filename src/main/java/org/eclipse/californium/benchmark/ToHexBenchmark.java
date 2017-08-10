c6f53980ab9d6be3e915bdfe24757974541a05ed
Simon Bernard <sbernard@sierrawireless.com>
2017-08-10 18:24:11 +0200

    Add boaks ToHex implemenation

--- src/main/java/org/eclipse/californium/benchmark/ToHexBenchmark.java
+++ src/main/java/org/eclipse/californium/benchmark/ToHexBenchmark.java
@@ -27,43 +27,58 @@ import org.openjdk.jmh.annotations.State;
 
 public class ToHexBenchmark {
 
-    @State(Scope.Thread)
-    public static class MyState {
+	@State(Scope.Thread)
+	public static class MyState {
 
-        public byte[] payload = new byte[256];
-        public Random r = new Random();
-        
-        @Setup(Level.Invocation)
-        public void prepare() {
-            r.nextBytes(payload);
-        }
-    }
-    
-    
-    @Benchmark
-    public void testScandiumToHex(MyState state) {
-        ByteArrayUtils.toHexString(state.payload);
-    }
-    
-    @Benchmark
-    public void testApacheToHex(MyState state) {
-    	Hex.encodeHex(state.payload).toString();
-    }
-    
-    @Benchmark
-    public void testStackOverFlow(MyState state) {
-    	bytesToHex(state.payload);
-    }
-    
-    
-    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for ( int j = 0; j < bytes.length; j++ ) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
+		public byte[] payload = new byte[256];
+		public Random r = new Random();
+
+		@Setup(Level.Invocation)
+		public void prepare() {
+			r.nextBytes(payload);
+		}
+	}
+
+	@Benchmark
+	public void testScandiumToHex(MyState state) {
+		ByteArrayUtils.toHexString(state.payload);
+	}
+
+	@Benchmark
+	public void testApacheToHex(MyState state) {
+		Hex.encodeHex(state.payload).toString();
+	}
+
+	@Benchmark
+	public void testStackOverFlowToHex(MyState state) {
+		bytesToHex(state.payload);
+	}
+
+	@Benchmark
+	public void testBoaksToHex(MyState state) {
+		bytesToHex(state.payload);
+	}
+
+	private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+	public static String bytesToHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
+	
+	private final static char[] BIN_TO_HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+	public static String boaksBytesToHex(byte[] byteArray) {
+		char[] bytesHexadecimal = new char[byteArray.length * 2];
+		for (int src = 0, dest = 0; src < byteArray.length; src++) {
+			int value = byteArray[src] & 0xFF;
+			bytesHexadecimal[dest++] = BIN_TO_HEX_ARRAY[value >>> 4];
+			bytesHexadecimal[dest++] = BIN_TO_HEX_ARRAY[value & 0x0F];
+		}
+		return new String(bytesHexadecimal);
+	}
 }
